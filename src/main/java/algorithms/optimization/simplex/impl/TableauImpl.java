fa7f60f83e1eb53c8bc06a75b5d416fb86bf0b2e
pascal <psn754@gmail.com>
2020-10-10 00:54:04 +0200

    fix style checks

--- src/main/java/algorithms/optimization/simplex/impl/TableauImpl.java
+++ src/main/java/algorithms/optimization/simplex/impl/TableauImpl.java
@@ -10,12 +10,13 @@ import javafx.util.Pair;
 /**
  * Basic implementation of the simplex algorithm. For more information have a look at
  * <a href="https://en.wikipedia.org/wiki/Simplex_algorithm">Wikipedia</a>
- *
+ * <p>
  * This algorithm solves linear optimization problems.
- *
+ * </p>
+ * <p>
  * When you are not able to find a fitting start solution you need to use the 2-phase method which
  * is not implemented yet.
- *
+ * </p>
  * @author thepn
  */
 public class TableauImpl implements Tableau {
