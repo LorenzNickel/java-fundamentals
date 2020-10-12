2596d477d228f1521c71050d0999575ae6363c95
davsc <dav.schneider03@gmail.com>
2020-10-13 01:05:44 +0200

    Formatting

--- src/test/java/algorithms/search/AStarTest.java
+++ src/test/java/algorithms/search/AStarTest.java
@@ -5,14 +5,14 @@ import algorithms.search.astar.model.Vector2;
 
 public class AStarTest {
 
-    public static void test() {
-        AStar astar = new AStar(30, new Vector2(0, 0), new Vector2(26, 20));
-        astar.setRandomBlocks(200);
-        try {
-            astar.calcPath();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        System.out.printf("\nTotal path cost: %f", astar.getTotalCost());
+  public static void test() {
+    AStar astar = new AStar(30, new Vector2(0, 0), new Vector2(26, 20));
+    astar.setRandomBlocks(200);
+    try {
+      astar.calcPath();
+    } catch (InterruptedException e) {
+      e.printStackTrace();
     }
+    System.out.printf("\nTotal path cost: %f", astar.getTotalCost());
+  }
 }
