d5c31ca5bbb2eb95e3699baa477e1e668385b02f
AtharvaKamble <atharvadkamble00@gmail.com>
2020-10-13 23:11:29 +0530

    feat: Added Selection sort

---
+++ src/test/java/algorithms/sort/SelectionSort.java
@@ -0,0 +1,37 @@
+package algorithms.sort;
+
+/**
+ * Static implementation of the selection sort algorithm
+ *
+ * @author AtharvaKamble
+ * @see <a href="https://www.geeksforgeeks.org/selection-sort/">Selection Sort (GeeksforGeeks)</a>
+ */
+public class SelectionSort {
+
+  private SelectionSort() { }
+
+  /**
+   * Sorts the array using selection sort algorithm
+   *
+   * @param <T>
+   * @param input
+   */
+  public static <T extends Comparable<T>> void selectionSort(T[] input) {
+    int minimum; 
+
+    for (int i = 0; i < input.length - 1; i++) {
+      minimum = i;
+      for (int j = i + 1; j < input.length; j++){
+        // Checks if we have a new minimum
+        if (a[j] < a[minimum])
+          minimum = j;
+        
+        // Swaps elements
+        T temp = input[i];
+        input[i] = input[minimum];
+        input[minimum] = temp;
+      }
+    }
+  }
+
+}
