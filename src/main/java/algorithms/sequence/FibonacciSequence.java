583f3e8d61c9fc0ddf22948ebbf33d9c75361f80
José Cunha <josefdcunha97@gmail.com>
2020-10-13 22:04:11 +0100

    Repair of the .ideia and fibonacci correction
    I repair the .ideia like in the master branch and resolve the fibonacci to be like in the documentation

--- src/main/java/algorithms/sequence/FibonacciSequence.java
+++ src/main/java/algorithms/sequence/FibonacciSequence.java
@@ -14,11 +14,11 @@ public class FibonacciSequence {
    * @return the number in postion n, if n is 0 or negative will return -1
    */
   public static int fibonacci(int n) {
-    if (n <= 0) {
+    if (n < 0) {
       return -1;
     }
-    if (n == 1 || n == 2) {
-      return n - 1;
+    if (n == 0 || n == 1) {
+      return n;
     }
     return fibonacci(n - 1) + fibonacci(n - 2);
   }
