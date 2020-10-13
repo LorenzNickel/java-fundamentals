876df0501880bc5e18e554ec956bc68342a30f1a
José Cunha <josefdcunha97@gmail.com>
2020-10-13 21:05:19 +0100

    Style correction

--- src/main/java/algorithms/sequence/FibonacciSequence.java
+++ src/main/java/algorithms/sequence/FibonacciSequence.java
@@ -13,9 +13,13 @@ public class FibonacciSequence {
    * @param n the position in Fibonacci Sequence
    * @return the number in postion n, if n is 0 or negative will return -1
    */
-  public static int fibonacci(int n){
-    if (n<=0) return -1;
-    if(n==1 || n==2) return n-1;
-    return fibonacci(n-1) + fibonacci(n-2);
+  public static int fibonacci(int n) {
+    if (n <= 0) {
+      return -1;
+    }
+    if (n == 1 || n == 2) {
+      return n - 1;
+    }
+    return fibonacci(n - 1) + fibonacci(n - 2);
   }
 }
