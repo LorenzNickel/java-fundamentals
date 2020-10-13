42f8dda8caec6524810e831528f2ba7a535e3187
Chandana V Kumar <chandanavkumar41428@gmail.com>
2020-10-13 15:29:20 +0530

    Create sum_of_two_matrices.java
    This program reads two matrices of the same number of rows and columns from the user, prints the given matrices as well as the sum.

---
+++ sum_of_two_matrices.java
@@ -0,0 +1,75 @@
+import java.util.Scanner;
+
+public class matrix {
+    public static void main(String[] args) {
+        int row,col;
+
+        Scanner sc=new Scanner(System.in);
+
+
+        System.out.print("Input number of rows: ");
+        row=sc.nextInt();
+        System.out.print("Input number of columns: ");
+        col=sc.nextInt();
+
+
+        int a[][]=new int[row][col];
+        int b[][]=new int[row][col];
+        int c[][]=new int[row][col];
+
+
+        System.out.println("Enter elements of matrix a:");
+        for(int i=0; i<row; i++)
+        {
+            for(int j=0; j<col; j++)
+            {
+
+                a[i][j]=sc.nextInt();
+            }
+        }
+
+        System.out.println("Enter elements of matrix b:");
+        for(int i=0; i<row; i++)
+        {
+            for(int j=0; j<col; j++)
+            {
+                
+                b[i][j]=sc.nextInt();
+            }
+        }
+
+
+        System.out.println("Matrix a:");
+        for(int i=0; i<row; i++){
+            for(int j=0; j<col; j++){
+                System.out.print(a[i][j] + "\t");
+            }
+            System.out.print("\n");
+        }
+
+
+        System.out.println("Matrix b:");
+        for(int i=0; i<row; i++){
+            for(int j=0; j<col; j++){
+                System.out.print(b[i][j] + "\t");
+            }
+            System.out.print("\n");
+        }
+
+
+        for(int i=0; i<row; i++){
+            for(int j=0; j<col; j++){
+                c[i][j]=a[i][j] + b[i][j];
+            }
+        }
+
+
+        System.out.println(":: Final Matrix:");
+        for(int i=0; i<row; i++){
+            for(int j=0; j<col; j++){
+                System.out.print(c[i][j] + "\t");
+            }
+            System.out.print("\n");
+        }
+    }
+}
