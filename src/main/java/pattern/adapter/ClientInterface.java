a2c59be5712d92c677a4654a3e3d4d626fa4482c
Aryaman19 <aryaman.thakur999888@gmail.com>
2020-10-26 10:08:39 +0530

    Added adapter pattern

---
+++ src/main/java/pattern/adapter/ClientInterface.java
@@ -0,0 +1,10 @@
+package pattern.adapter;
+
+/**
+ * @author Aryaman19
+ */
+
+public interface ClientInterface {
+    public String  clientRequest();
+    public String clientMethod();
+}
---
+++ src/main/java/pattern/adapter/ServiceInterface.java
@@ -0,0 +1,10 @@
+package pattern.adapter;
+
+/**
+ * @author Aryaman19
+ */
+
+public interface ServiceInterface {
+    public String serviceRequest();
+    public String serviceMethod();
+}
